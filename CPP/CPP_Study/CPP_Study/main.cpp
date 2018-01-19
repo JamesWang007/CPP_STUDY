47b8d6458041c6cf3c185813f6f3c1d77bdd2589
JamesWang007 <wanghaiyang20@gmail.com>
2018-01-19 14:45:53 -0800

    cmit

---
+++ CPP/CPP_Study/CPP_Study/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+#include "c_str.h"
+
+using namespace std;
+
+
+int main()
+{
+	/*
+	string s("0123456789");
+
+	for (auto c : s)
+	{
+		cout << c << endl;
+	}
+	*/
+
+	test_c_str();
+
+
+	return 0;
+}
